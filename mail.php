787e25bd205a9c14e2a7960aab75b2e07dfda0cd
Stas <sromanenko91@gmail.com>
2017-11-08 09:42:06 +0300

    UPD

--- mail.php
+++ mail.php
@@ -1,56 +1,30 @@
 <?php
-$name=$_POST['name'];
-$email=$_POST['email'];
-$mess=$_POST['mess'];
-if (isset($name))
-	{
-	$name = substr($name,0,20); //Не может быть более 20 символов
-		if (empty($name))
-			{
-			echo "<center><b>Не указано имя !!!<p>";
-			echo "<a href=index.html>Вернуться и правильно заполнить форму.</a>";
-			exit;
-			}		
-	}
-else
-	{
-	$name = "не указано";
-	}
-if (isset ($email))
-	{
-	$email = substr($email,0,20); //Не может быть более 20 символов
-	if (empty($email))
-		{
-		echo "<center><b>Не указан e-mail !!!<p>";
-		echo "<a href=index.php>Вернуться и правильно заполнить форму.</a>";
-		exit;
-		}
-	}
-else
-	{
-	$email = "не указано";
-	}
-if (isset ($mess))
-	{
-	$mess = substr($mess,0,1000); //Не может быть более 1000 символов
-	if (empty($mess))
-		{
-		echo "<center><b>Сообщение не написано !!!<p>";
-		echo "<a href=index.html>Вернуться и правильно заполнить форму.</a>";
-		exit;
-		}
-	}
-else
-{
-$mess = "не указано";
+
+/** @link:http://stackoverflow.com/questions/9154719/check-whether-post-value-is-empty */
+
+if ("" == trim($_POST['name'])) {
+    echo "<b>Не указано имя !!!</b>"
+        . "<a href=index.html>Вернуться и правильно заполнить форму.</a>";
+    exit;
 }
-$i = "не указано";
-if ($name == $i AND $email == $i AND $mess == $i)
-{
-echo "Ошибка ! Скрипту не были переданы параметры !";
-exit;
+
+if ("" == trim($_POST['email'])) {
+    echo "<b>Не указан e-mail !!!</b>"
+        . "<a href=index.html>Вернуться и правильно заполнить форму.</a>";
+    exit;
 }
-$to = "stas@estudo.ru";  /*Мой адрес!*/
+
+if ("" == trim($_POST['mess'])) {
+    echo "<b>Сообщение не написано !!!</b>"
+        . "<a href=index.html>Вернуться и правильно заполнить форму.</a>";
+    exit;
+}
+
+$name = substr($_POST['name'], 0, 30); //Не может быть более 30 символов
+$email = substr($_POST['email'], 0, 30); //Не может быть более 30 символов
+$mess = substr($_POST['mess'], 0, 1000); //Не может быть более 1000 символов
+
+$to = "elpsk@bk.ru";  /** Мой адрес!*/
 $subject = "Сообщение с вашего интернет-сайта";
 $subject = iconv("UTF-8", "KOI8-R", "$subject"); /*тут короч с кодировкой прост беда, почта не читает*/
 $message = "Имя пославшего: $name \nЭлектронный адрес: $email \nСообщение: $mess";
@@ -66,7 +40,7 @@ $file = 'huiysoria.txt';
 $text = "$message \n";
 file_put_contents($file, $text, FILE_APPEND | LOCK_EX);
 // отправляю в почту
-mail ($to,$subject,$message) or print "Не могу отправить письмо !!!";
-echo "<center><b>Спасибо за отправку Вашего сообщения. <a href=index.html>Нажмите</a>, что бы вернуться на главную... <br>Либо перейти на один из наших сайтов: <a href=\"http://www.estudo.ru/\" target=\"_blank\">ESTUDO.RU</a>,  <a href=\"http://xn--h1aaljkbdi.xn--p1ai/\" target=\"_blank\">СИМИСТОР.РФ</a> ";
-exit;
-?>
+mail($to, $subject, $message) or print "Не могу отправить письмо !!!";
+/*echo "<center><b>Спасибо за отправку Вашего сообщения. <a href=index.html>Нажмите</a>, что бы вернуться на главную... <br>Либо перейти на один из наших сайтов: <a href=\"http://www.estudo.ru/\" target=\"_blank\">ESTUDO.RU</a>,  <a href=\"http://xn--h1aaljkbdi.xn--p1ai/\" target=\"_blank\">СИМИСТОР.РФ</a> ";
+http://xn--h1aaljkbdi.xn--p1ai/index.php?option=com_content&view=article&id=29 */
+
---
+++ stasmodal.js
@@ -0,0 +1,88 @@
+/**
+ * Created by stas on 09.11.2016.
+ */
+jQuery(document).ready(function () {
+    jQuery('.btnstas').click(function () {
+        jQuery('#overlay_stas').fadeIn(400,
+            function () {
+                jQuery('#modal_stas')
+                    .css('display', 'block')
+                    .animate({opacity: 1, top: '50%'}, 200);
+            }
+        );
+    });
+
+    jQuery('.btnstas2').click(function () {
+        jQuery('#overlay_stas').fadeIn(400,
+            function () {
+                jQuery('#modal_stas')
+                    .css('display', 'block')
+                    .animate({opacity: 1, top: '50%'}, 200);
+            }
+        );
+    });
+
+    jQuery("<div id='modal_stas'> " +
+        "<button id='modal_close_stas'>×</button>" +
+        "<div><p class='stasformtext_stasmodal'>Заполните форму, что бы узнать актуальные цены!</p></div>"+
+        "<form id='stas_modal_form' class='form-horizontal' method='post' action='javascript:void(0)'>" +
+            "<div class='form-groups_stasmodal'>" +
+                "<label class='labelstas_stasmodal' for='name'>Имя:</label>" +
+                "<div><input id='name_stasmodal' name='name' required='' type='text' placeholder='Введите Имя*' /></div>" +
+             "</div>" +
+            "<div class='form-groups_stasmodal'>" +
+                "<label class='labelstas_stasmodal' for='email'>Email:</label>" +
+                "<div><input id='email_stasmodal' class='' name='email' required='' type='email' placeholder='Введите email*' /></div>" +
+            "</div>" +
+            "<div class='form-groups_stasmodal'>" +
+                "<label class='labelstas_stasmodal' for='textarea'>Текст:</label>" +
+                "<div><textarea id='textarea_stasmodal' class='' name='mess' rows='3' required='' placeholder='Какой товар вас интересует?*'></textarea></div>" +
+            "</div>" +
+            "<div class='form-groups_stasmodal'>" +
+                "<div class='stasformbut_stasmodal'><input id='stas_button' type='submit' value='Отправить' /></div>" +
+            "</div>" +
+        "</form>" +
+      "</div>" +
+    "<div id='overlay_stas'></div>" +
+    "<div id='stas_otpravleno'><button id='otpravleno_close_stas'>×</button>" +
+            "<div class='div_otpr'><p class='stas_otpravleno_zagol'>Ваша заявка отправленна!</p>" +
+                "<p class='stas_otpravleno_text'>В течении дня менеджер ответит Вам на указанную почту.</p></div>" +
+            "<div id='div_ok'><button id='otpr_ok'>ОК</button></div>" +
+        "</div>").insertAfter(".footer");
+
+    jQuery('#modal_close_stas, #overlay_stas, #otpravleno_close_stas, #div_ok').click(function () {
+        jQuery('#modal_stas, #overlay_stas, #stas_otpravleno').css('display', 'none');
+    });
+    jQuery('#stas_modal_form').submit(function () {
+        jQuery.post('/promo/mail.php', {
+            name: jQuery('#name_stasmodal').val(),
+            email: jQuery('#email_stasmodal').val(),
+            mess: jQuery('#textarea_stasmodal').val()
+        })
+            .done(function() {
+                jQuery('#modal_stas').css('display', 'none');
+                jQuery('#stas_otpravleno').css('display', 'block').animate({opacity: 1, top: '50%'}, 200);
+                jQuery('#name_stasmodal, #email_stasmodal, #textarea_stasmodal').val('');
+            })
+            .fail(function(){
+                alert ("Что-то пошло не так. Повторите пожалуйста отправку сообщения или напишите свою заявку нам на почту: info@estudo.ru");
+            });
+    });
+    jQuery('#stas_modal_form2').submit(function () {
+        jQuery.post('/promo/mail.php', {
+            name: jQuery('#name_stasmodal2').val(),
+            email: jQuery('#email_stasmodal2').val(),
+            mess: jQuery('#textarea_stasmodal2').val()
+        })
+            .done(function() {
+                jQuery('#overlay_stas').fadeIn(400,
+                    function () {
+                    jQuery('#stas_otpravleno').css('display', 'block').animate({opacity: 1, top: '50%'}, 200);
+                    });
+                jQuery('#name_stasmodal2, #email_stasmodal2, #textarea_stasmodal2').val('');
+            })
+            .fail(function(){
+                alert ("Что-то пошло не так. Повторите пожалуйста отправку сообщения или напишите свою заявку нам на почту: info@estudo.ru");
+            });
+    });
+});
